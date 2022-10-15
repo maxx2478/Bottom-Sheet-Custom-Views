30004c6ca2c5f77bbbc00f59493f5f83a5419b1b
maxx2478 <mkpatil24@gmail.com>
2022-10-15 17:13:42 +0530

    bug fixes

--- app/src/main/java/com/manohar/usefullibraries/MainActivity.kt
+++ app/src/main/java/com/manohar/usefullibraries/MainActivity.kt
@@ -2,11 +2,26 @@ package com.manohar.usefullibraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.manohar.useful_views.SearchModel
+import com.manohar.useful_views.SelectionModel
+import com.manohar.useful_views.SelectorDialog
+import java.nio.channels.Selector
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = arrayListOf<SelectionModel>()
+        list.add(SelectionModel(data = SearchModel("1", "name1"), isSelected = false))
+        list.add(SelectionModel(data = SearchModel("2", "name12"), isSelected = false))
+        list.add(SelectionModel(data = SearchModel("3", "name13"), isSelected = false))
+
+        val dialog = SelectorDialog.showSelectorDialog(this, list){
+            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+        }.show()
+
 
 
 
--- useful_views/src/main/java/com/manohar/useful_views/SelectorDialog.kt
+++ useful_views/src/main/java/com/manohar/useful_views/SelectorDialog.kt
@@ -13,79 +13,82 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 
-class SelectorDialog(var onSubmit: ((List<SelectionModel?>) -> Unit)? = null) {
-    lateinit var selectionAdapter: SelectionAdapter
+class SelectorDialog() {
 
-    fun showSelectorDialog(context: Context, RealLists: ArrayList<SelectionModel>) {
-        val list = RealLists
-        val factory = LayoutInflater.from(context)
-        val deleteDialogView: View =
-            factory.inflate(R.layout.dialog_selector, null)
-        val dialog = AlertDialog.Builder(context).create()
-        dialog.setCancelable(true)
-        dialog.setView(deleteDialogView)
-        dialog.setCanceledOnTouchOutside(true)
+    companion object{
+        lateinit var selectionAdapter: SelectionAdapter
 
 
-        val selectorRV: RecyclerView? =
-            deleteDialogView.findViewById<RecyclerView>(R.id.selectorRV)
-
-        val done: MaterialButton? =
-            deleteDialogView.findViewById<MaterialButton>(R.id.done)
-
-        val search: TextInputEditText? =
-            deleteDialogView.findViewById<TextInputEditText>(R.id.search_bar)
-
-
-
-        selectorRV?.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-        selectorRV?.adapter = selectionAdapter
+        fun showSelectorDialog(context: Context, RealLists: ArrayList<SelectionModel>,  onSubmit: ((List<SelectionModel?>) -> Unit)? = null) : AlertDialog {
+            val list = RealLists
+            val factory = LayoutInflater.from(context)
+            val deleteDialogView: View =
+                factory.inflate(R.layout.dialog_selector, null)
+            val dialog = AlertDialog.Builder(context).create()
+            dialog.setCancelable(true)
+            dialog.setView(deleteDialogView)
+            dialog.setCanceledOnTouchOutside(true)
 
 
-        selectionAdapter.updateData(list)
-        selectionAdapter.onMarked = { item, isChecked ->
-            list.find { list -> list.data?.equals(item?.data) == true }?.isSelected = isChecked
-        }
+            val selectorRV: RecyclerView? =
+                deleteDialogView.findViewById<RecyclerView>(R.id.selectorRV)
+
+            val done: MaterialButton? =
+                deleteDialogView.findViewById<MaterialButton>(R.id.done)
+
+            val search: TextInputEditText? =
+                deleteDialogView.findViewById<TextInputEditText>(R.id.search_bar)
 
 
-        done?.setOnClickListener {
-            dialog.dismiss()
-            onSubmit?.invoke(list)
-        }
+
+            selectionAdapter = SelectionAdapter()
+            selectorRV?.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            selectorRV?.adapter = selectionAdapter
 
 
-        search?.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
+            selectionAdapter.updateData(list)
+            selectionAdapter.onMarked = { item, isChecked ->
+                list.find { list -> list.data?.equals(item?.data) == true }?.isSelected = isChecked
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (!p0.isNullOrEmpty()) {
-                    val temp = list.filter {
-                        it.data?.name?.contains(
-                            p0.toString(),
-                            ignoreCase = true
-                        ) == true
-                    }
-                    selectionAdapter.updateData(ArrayList(temp))
-                } else {
-                    selectionAdapter.updateData(ArrayList(list))
+
+            done?.setOnClickListener {
+                dialog.dismiss()
+                onSubmit?.invoke(list.filter {item-> item.isSelected })
+            }
+
+
+            search?.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
                 }
-            }
 
-            override fun afterTextChanged(p0: Editable?) {
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (!p0.isNullOrEmpty()) {
+                        val temp = list.filter {
+                            it.data?.name?.contains(
+                                p0.toString(),
+                                ignoreCase = true
+                            ) == true
+                        }
+                        selectionAdapter.updateData(ArrayList(temp))
+                    } else {
+                        selectionAdapter.updateData(ArrayList(list))
+                    }
+                }
 
-            }
+                override fun afterTextChanged(p0: Editable?) {
 
-        })
+                }
 
-        if (context is Fragment) {
-            if ((context as Fragment).isAdded)
-                dialog.show()
-        } else
-            dialog.show()
+            })
+
+            return dialog
+        }
     }
+
+
 
 
 }
